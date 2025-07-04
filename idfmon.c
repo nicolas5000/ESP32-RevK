f4123df37b6ba05077a9eddfdb24438604fd60a6
RevK <github@e.gg>
2025-07-04 09:36:03 +0100

    RST

--- idfmon.c
+++ idfmon.c
@@ -70,6 +70,11 @@ main (int argc, const char *argv[])
             printf ("Empty chip, ready to flash\n");
             return 0;
          }
+         if (strstr (line, "rst:0x3 (RTC_SW_SYS_RST),boot:0x8 (SPI_FAST_FLASH_BOOT)\n"))
+         {
+            printf ("Resetting, re-flash.\n");
+            return 0;
+         }
          printf ("%s", line);
       }
       close (fd);
